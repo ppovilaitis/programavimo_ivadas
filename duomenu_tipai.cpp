fb3c3ec8baa74c903a201846027a5d7d27631b9f
Pranas Povilaitis <povilaitispranas@gmail.com>
2021-09-18 22:46:39 +0300

    duomenu tipai

---
+++ duomenu_tipai.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a;
+	unsigned char b;
+	signed char c;
+	int d;
+	unsigned int e;
+	signed int f;
+	unsigned short int g;
+	signed short int h;
+	long int i;
+	signed long int j;
+	unsigned long int k;
+	float l;
+	double n;
+	long double m;
+
+	cout << "Iveskite char.:\n";
+	cin >> a;
+	cout << "Jusu ivestas char. yra: \n"
+		 << a << "\n";
+
+	cout << "Iveskite unsigned char.:\n";
+	cin >> b;
+	cout << "Jusu ivestas unsigned char. yra: \n"
+		 << b << "\n";
+
+	cout << "Iveskite signed char.:\n";
+	cin >> c;
+	cout << "Jusu ivestas signed char. yra: \n"
+		 << c << "\n";
+
+	cout << "Iveskite int.:\n";
+	cin >> d;
+	cout << "Jusu ivestas int. yra: \n"
+		 << d << "\n";
+
+	cout << "Iveskite unsigned int.:\n";
+	cin >> e;
+	cout << "Jusu ivestas unsigned int. yra: \n"
+		 << e << "\n";
+
+	cout << "Iveskite signed int.:\n";
+	cin >> f;
+	cout << "Jusu ivestas signed int. yra: \n"
+		 << f << "\n";
+
+	cout << "Iveskite unsigned short int.:\n";
+	cin >> g;
+	cout << "Jusu ivestas unsigned short int. yra: \n"
+		 << g << "\n";
+
+	cout << "Iveskite signed short int.:\n";
+	cin >> h;
+	cout << "Jusu ivestas signed short int. yra: \n"
+		 << h << "\n";
+
+	cout << "Iveskite long int.:\n";
+	cin >> i;
+	cout << "Jusu ivestas long int. yra: \n"
+		 << i << "\n";
+
+	cout << "Iveskite signed long int.:\n";
+	cin >> j;
+	cout << "Jusu ivestas signed long int. yra: \n"
+		 << j << "\n";
+
+	cout << "Iveskite unsigned long int.:\n";
+	cin >> k;
+	cout << "Jusu ivestas unsigned long int. yra: \n"
+		 << k << "\n";
+
+	cout << "Iveskite float.:\n";
+	cin >> l;
+	cout << "Jusu ivestas float. yra: \n"
+		 << l << "\n";
+
+	cout << "Iveskite double.:\n";
+	cin >> n;
+	cout << "Jusu ivestas double. yra: \n"
+		 << n << "\n";
+
+	cout << "Iveskite long double.:\n";
+	cin >> m;
+	cout << "Jusu ivestas long double. yra: \n"
+		 << m << "\n";
+
+	return 0;
+	 
